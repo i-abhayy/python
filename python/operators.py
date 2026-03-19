84ba3e8e25d91c7677e7571c5eb319fb302fc2d1
Abhay <abhay.hack4@gmail.com>
2026-03-19 09:59:36 +0530

    upload all python files

---
+++ python/operators.py
@@ -0,0 +1,159 @@
+##ARITHMETIC OPERATORS (5)
+
+'''Add 10 and 5, store in a variable.
+
+Subtract 7 from 20, store in a variable.
+
+Multiply 8 and 6, store in a variable.
+
+Divide 15 by 2, store in a variable.
+
+Find remainder when 17 is divided by 5, store in a variable.'''
+
+##COMPARISON OPERATORS (5)
+
+'''Check if 10 is greater than 5, store result in a variable.
+
+Check if 7 is equal to 7, store result in a variable.
+
+Check if 9 is not equal to 5, store result in a variable.
+
+Check if 8 is less than or equal to 8, store result in a variable.
+
+Check if 15 is greater than 10 and less than 20, store result in a variable.'''
+
+##LOGICAL OPERATORS (5)
+
+'''Calculate True and False and store result in a variable.
+
+Calculate True or False and store result in a variable.
+
+Calculate not True and store result in a variable.
+
+Calculate not (True and False) and store result in a variable.
+
+Calculate (True or False) and (not False) and store result in a variable.'''
+
+#ASSIGNMENT OPERATORS (5)
+
+'''Increase x = 10 by 5 using assignment operator.
+
+Subtract 4 from x = 20 using assignment operator.
+
+Multiply x = 7 by 3 using assignment operator.
+
+Divide x = 50 by 5 using assignment operator.
+
+Increase x = 9 by 2, then multiply by 3 using assignment operators.'''
+
+#BITWISE OPERATORS (5)
+
+'''Calculate a & b for a=5 and b=3.
+
+Calculate a | b for a=5 and b=3.
+
+Calculate a ^ b for a=5 and b=3.
+
+Calculate ~a for a=5.
+
+Left shift a=5 by 2 and right shift a=5 by 1, store both results.'''
+
+
+'''Airthmetic operators'''
+'''program1'''
+x=5
+y=10
+print(x+y) #addition    
+
+'''program2'''
+result=7-20
+
+'''program3'''
+mul=8*6 #multiplication
+
+'''program4'''
+div=15/2 #division
+
+'''program5'''
+mod=17%5 #modulus
+
+'''comparison/relational operators'''
+'''program6'''
+res=10>5 #greater than
+
+'''program7'''
+check= 7==7 #equal to
+
+'''program8'''
+not_equal= 9!=5  #not equal to
+
+'''program9'''
+ans= 8<=8 #less than or equal to
+
+'''program10'''
+res= 15>10 and 15<20 #logical and
+
+'''logical operators'''
+'''program11'''
+res = True and False  #logical and
+
+'''program12'''
+res = True or False  #logical or
+
+'''program13'''
+res = not True  #logical not
+
+'''program14'''
+res= not (True and False) #logical not
+
+'''program15'''
+res = (True or False) and (not False) #logical operators with parentheses
+
+'''assignment operators'''
+
+'''program16'''
+x=5
+x+=5 #assignment operator
+
+'''program17'''
+x=20
+x-=4 #assignment operator
+
+'''program18'''
+x=7
+x*=3 #assignment operator
+
+'''program19'''
+x=50
+x/=5 #assignment operator
+
+'''program20'''
+x=9
+x+=2
+x*=3 #assignment operator with multiple operations
+
+'''bitwise operators'''
+
+'''program21'''
+a=5
+b=3 
+res=a & b #bitwise AND
+
+'''program22'''
+a=5
+b=3
+res=a|b
+
+'''program23'''
+a=5
+b=3
+res=a^b #bitwise XOR
+
+'''program24'''
+a=5
+res=~a #bitwise NOT 
+
+'''program25'''
+a=5
+left_shift=a<<2 #bitwise left shift
+right_shift=a>>1 #bitwise right shift    
